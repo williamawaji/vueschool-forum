ff8c8ada6076e766122e84d74b3e562b9e869986
William Awaji <william.hiroshi@gmail.com>
2018-07-29 01:33:00 -0300

    Adding Categories components in the forum application. - Created PageCategory and configured in router config. - Created CategoryList and CategoryListItem - Edited ForumList html - Edited PageHome to use CategoryList

---
+++ src/components/CategoryList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <CategoryListItem v-for="category in categories" :key="category['.key']" :category="category" />
+    </div>
+</template>
+
+<script>
+import CategoryListItem from './CategoryListItem'
+export default {
+  components: {
+    CategoryListItem
+  },
+  props: {
+    categories: {
+      required: true,
+      type: Array
+    }
+  }
+}
+</script>
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,25 +1,22 @@
 <template>
   <div class="col-full">
     <h1>Welcome to the forum</h1>
-    <ForumList :forums="forums"/>
+    <CategoryList :categories="categories"/>
   </div>
 </template>
 
 <script>
 import sourceData from '@/data'
-import ForumList from '@/components/ForumList'
+import CategoryList from '@/components/CategoryList'
 console.log(sourceData)
 
 export default {
   components: {
-    ForumList
+    CategoryList
   },
   data () {
     return {
-      forums: Object.values(sourceData.forums),
-      threads: Object.values(sourceData.threads),
-      posts: sourceData.posts,
-      users: sourceData.users
+      categories: Object.values(sourceData.categories)
     }
   }
 }
