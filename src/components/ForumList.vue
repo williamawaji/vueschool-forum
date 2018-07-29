5de433c672c55ed0b6355d338f50f14dc72ecceb
William Awaji <william.hiroshi@gmail.com>
2018-07-28 22:32:38 -0300

    Forum Components - Created ForumList component - Created ForumListItem component - Created PageForum - Added PageForum to router-link

---
+++ src/components/ForumList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="forum-list">
+        <h2 class="list-title">
+            <a href="#">Forums</a>
+        </h2>
+        <ForumListItem v-for="forum in forums" :key="forum['.key']" :forum="forum" />
+    </div>
+</template>
+
+<script>
+import ForumListItem from './ForumListItem'
+export default {
+  components: {
+    ForumListItem
+  },
+  props: {
+    forums: {
+      requred: true,
+      type: Array
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,21 +1,22 @@
 <template>
   <div class="col-full">
     <h1>Welcome to the forum</h1>
-    <ThreadList :threads="threads"/>
+    <ForumList :forums="forums"/>
   </div>
 </template>
 
 <script>
 import sourceData from '@/data'
-import ThreadList from '@/components/ThreadList'
+import ForumList from '@/components/ForumList'
 console.log(sourceData)
 
 export default {
   components: {
-    ThreadList
+    ForumList
   },
   data () {
     return {
+      forums: Object.values(sourceData.forums),
       threads: Object.values(sourceData.threads),
       posts: sourceData.posts,
       users: sourceData.users
