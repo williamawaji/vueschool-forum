3c388988c67d87aabcee67facc3117ecdb697607
William Awaji <william.hiroshi@gmail.com>
2018-08-04 18:17:47 -0300

    User Profile Page - Created user profile page - Adjusrt navbar to redirect to user page information.

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Home from '@/pages/PageHome'
 import ThreadShow from '@/pages/PageThreadShow'
 import Forum from '@/pages/PageForum'
 import Category from '@/pages/PageCategory'
+import Profile from '@/pages/PageProfile'
 import NotFound from '@/pages/PageNotFound'
 
 Vue.use(Router)
@@ -32,6 +33,11 @@ export default new Router({
       name: 'Category',
       component: Category,
       props: true
+    },
+    {
+      path: '/me',
+      name: 'Profile',
+      component: Profile
     },
     {
       path: '*',
