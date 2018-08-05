67d5ef329062be439b434e36c538b1096a73020e
William Awaji <william.hiroshi@gmail.com>
2018-08-05 00:30:14 -0300

    Fixing bus - Remove publishedAt date from component This information is set on action.

--- src/components/PostEditor.vue
+++ src/components/PostEditor.vue
@@ -31,7 +31,6 @@ export default {
     save () {
       const post = {
         text: this.text,
-        publishedAt: Math.floor(Date.now() / 1000),
         threadId: this.threadId
       }
       this.text = ''
