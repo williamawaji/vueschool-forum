02f7c77cdef721c13205e9d262e68f3606f15256
William Awaji <william.hiroshi@gmail.com>
2018-08-13 00:15:43 -0300

    workflow improvements - Installed firebase tools - Added new script to reset base database by comand line - Installed dotenv package - Configured .env file with .env.example file - Adjusted prod.env.js file and main to use env config

--- src/main.js
+++ src/main.js
@@ -12,12 +12,12 @@ Vue.config.productionTip = false
 
 // Initialize Firebase
 var config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: ''
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABSE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_ID
 }
 firebase.initializeApp(config)
 
@@ -27,5 +27,8 @@ new Vue({
   router,
   store,
   template: '<App/>',
-  components: { App }
+  components: { App },
+  beforeCreate () {
+    store.dispatch('fetchUser', { id: store.state.authId })
+  }
 })
