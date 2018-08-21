449eda20baeb62990b85a90ff2df9abf384b2285
William Awaji <william.hiroshi@gmail.com>
2018-08-21 00:42:43 -0300

    Implemented firebase authentication on RegisterPage. - Created new action to create user auth by email and pass - Adjusted  created user with auth key. - Adjusted PageRegister

--- src/store/actions.js
+++ src/store/actions.js
@@ -71,28 +71,31 @@ export default {
         })
     })
   },
-  createUser ({ commit, state }, { email, name, username, avatar = null }) {
+  createUser ({ commit, state }, { id, email, name, username, avatar = null }) {
     return new Promise((resolve, reject) => {
       const registeredAt = Math.floor(Date.now() / 1000)
       const usernameLower = username.toLowerCase()
       email = email.toLowerCase()
       const user = { avatar, email, name, username, usernameLower, registeredAt }
 
-      const userId = firebase
-        .database()
-        .ref('users')
-        .push().key
-
       firebase
         .database()
         .ref('users')
-        .child(userId)
+        .child(id)
         .set(user)
         .then(() => {
-          commit('setItem', { resource: 'users', item: user, id: userId })
-          resolve(state.users[userId])
+          commit('setItem', { resource: 'users', id: id, item: user })
+          resolve(state.users[id])
         })
     })
+  },
+  registerUserWithEmailAndPassword ({ dispatch }, { email, password, name, username, avatar = null }) {
+    return firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(user => {
+        return dispatch('createUser', { id: user.uid, email, name, username, password, avatar })
+      })
   },
   updateThread ({ commit, state, dispatch }, { title, text, id }) {
     return new Promise((resolve, reject) => {
